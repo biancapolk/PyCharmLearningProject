e5d3838af4a24d064885d85e2ac938673dabf13d
Bia Polk <bianca@shipwell.com>
2021-05-11 02:17:47 -0500

    Added more methods to Deck Added Player class

--- war_game/war.py
+++ war_game/war.py
@@ -30,19 +30,55 @@ class Deck:
                 created_card = Card(suit, rank)
                 self.all_cards.append(created_card)
 
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+    def remove_one(self):
+        pass
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            # List of multiple card objects
+            self.all_cards.extend(new_cards)
+        else:
+            # For a single card object
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
 
 if __name__ == "__main__":
-    try:
+    # Creating a new instance of the Deck class
+    new_deck = Deck()
+    # first_card = new_deck.all_cards[-1]
+    # print(first_card)
 
-        five_of_hearts = Card("Hearts", 'Five')
-    except:
-        print("Sorry, something went wrong. Check that the title of the suit and rank are capitalized")
-    else:
-        print(five_of_hearts.value)
 
-new_deck = Deck()
-first_card = new_deck.all_cards[-1]
-print(first_card)
 
-for card_object in new_deck.all_cards:
-    print(card_object)
+    # try:
+    #
+    #     five_of_hearts = Card("Hearts", 'Five')
+    # except:
+    #     print("Sorry, something went wrong. Check that the title of the suit and rank are capitalized")
+    # else:
+    #     print(five_of_hearts.value)
+
+
+
+    # Loop through all of the cards in and new deck and print each card object
+    # for card_object in new_deck.all_cards:
+    #   print(card_object)
+
+    #
+    new_deck.shuffle()
+    mycard = new_deck.deal_one()
+    print(mycard)
+    print(new_deck.all_cards[0])
+    new_player = Player("Jose")
+    print(new_player)
