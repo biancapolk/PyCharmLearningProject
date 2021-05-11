67026a4efa7d89790eaea6d15a55310fe671aded
Bia Polk <bianca@shipwell.com>
2021-05-11 13:13:30 -0500

    definingg a simple cube function to use

--- decorators_tutorial/decorators.py
+++ decorators_tutorial/decorators.py
@@ -6,3 +6,14 @@ def calc_square(numbers):
     return result
 
 out_square = calc_square(array)
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
+
+array = range(1,100000)
+out_square = calc_square(array)
+out_cube = calc_cube(array)
